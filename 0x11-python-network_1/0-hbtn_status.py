f8424343cdbc54c504f6f79fe15b85d98ba0da5d
tebogo-gema <gematebogo@gmail.com>
2024-01-07 10:48:40 -0800

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
-
+import urllib.error
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+            html = response.read()
+            print('Body response:')
+            print('\t- type: {}'.format(type(html)))
+            print('\t- content: {}'.format(html))
+            print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    except urllib.error.URLError as e:
+        print('Error: {}'.format(e))
